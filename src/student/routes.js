7a5ffa68a88a7f74d34914e07ecb9c2f07ad2bab
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-12 19:23:10 +0300

    feat(Server.js): Add a middle to get json from our endpoints

--- src/student/routes.js
+++ src/student/routes.js
@@ -1,9 +1,13 @@
-const {Router} = require('express')
-
+const {Router} = require('express');
+const controller = require('./controller');
 const router = Router();
 
-router.get('/', (req, res)=>{
-    res.send('using api route');
-})
+
+
+
+router.get('/', controller.getStudents)
+
+
+
 
 module.exports = router;
